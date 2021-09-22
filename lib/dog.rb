415e3dd55374efd4d40860c7835beac0a17153b8
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-22 12:15:00 -0400

    commit tree 95badb5e9e1c23db220d65c9ac1abe6eaf34d090

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,34 @@
 class Dog
+  #setter method
+  def name=(dogs_name)
+    #assign the @name instance variable a value to whatever is passed as an argument
+    @this_dogs_name = dogs_name
+  end
+
+  #getter method
+  def name
+    #access the data from the @name instance variable and return it
+    @this_dogs_name
+  end 
 
 end
+
+
+#call the Dog class with the value of "lassie"
+lassie = Dog.new
+
+#calling lassie to the name method to equal "Lassie"
+lassie.name = "Lassie"
+
+#call the Dog class method
+puts lassie.name
+
+# here we have two instance methods! 
+  # 1. name =
+   # takes in an argument of a dog's name and assigns that value to a variable => this_dogs_name
+
+  # 2. name
+   # is responsible for reporting the name 
+
+
+# Uh-oh the name method doesn't know about the this_dogs_name variable in the name = because it is a local variable. We need to solve this with an instance variable. 
